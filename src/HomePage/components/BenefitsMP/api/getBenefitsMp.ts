82a2eaed33d8122de223fa683305c4aaa9cf632c
Jeronimo Pratto <jeropratto@yahoo.com.ar>
2023-06-28 14:44:24 -0300

    modified url images

--- src/HomePage/components/BenefitsMP/api/getBenefitsMp.ts
+++ src/HomePage/components/BenefitsMP/api/getBenefitsMp.ts
@@ -2,28 +2,27 @@ const getBenefitsMPMock = () => {
 	const benefitsMPMock = [
 		{
 			urlImgBackground:
-				'src/HomePage/components/BenefitsMP/images/multicontent.jpg',
+				'HomePage/components/BenefitsMP/images/multicontent.jpg',
 			alt: 'Disney+ y Star+',
 			urlIcon:
-				'src/HomePage/components/BenefitsMP/images/comboplus-square-multicontent.jpg',
+				'HomePage/components/BenefitsMP/images/comboplus-square-multicontent.jpg',
 			description:
 				'<p style="font-weight:bold">Sin cargo con el nivel 6</p> <p>Disney+ y Star+</p>',
 			bgColor:
 				'linear-gradient(0deg, rgb(8, 19, 48) 0%, rgba(8, 19, 48, 0) 100%);',
 		},
 		{
-			urlImgBackground: 'src/HomePage/components/BenefitsMP/images/HBOMAX.jpg',
+			urlImgBackground: 'HomePage/components/BenefitsMP/images/HBOMAX.jpg',
 			alt: 'hbo max',
-			urlIcon: 'src/HomePage/components/BenefitsMP/images/hbomax-icon.png',
+			urlIcon: 'HomePage/components/BenefitsMP/images/hbomax-icon.png',
 			description: `<span>7 DÍAS GRATIS</span> <p style="font-weight:bold">Hasta 30% OFF</p> <p>HBO Max</p>`,
 			bgColor:
 				'linear-gradient(0deg, rgb(60, 3, 78) 0%, rgba(60, 3, 78, 0) 100%)',
 		},
 		{
-			urlImgBackground:
-				'src/HomePage/components/BenefitsMP/images/paramount.jpg',
+			urlImgBackground: 'HomePage/components/BenefitsMP/images/paramount.jpg',
 			alt: 'paramount+',
-			urlIcon: 'src/HomePage/components/BenefitsMP/images/paramount-icon.png',
+			urlIcon: 'HomePage/components/BenefitsMP/images/paramount-icon.png',
 			description:
 				'<span>7 DÍAS GRATIS</span> <p style="font-weight:bold">Hasta 30% OFF</p> <p>Paramount+</p>',
 			bgColor:
