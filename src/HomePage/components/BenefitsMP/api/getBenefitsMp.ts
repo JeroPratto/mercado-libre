ac2d61aedad9b9a07e3f449d947cb255ac164ac2
Jeronimo Pratto <jeropratto@yahoo.com.ar>
2023-06-28 21:30:46 -0300

    modified url images

--- src/HomePage/components/BenefitsMP/api/getBenefitsMp.ts
+++ src/HomePage/components/BenefitsMP/api/getBenefitsMp.ts
@@ -1,26 +1,26 @@
 const getBenefitsMPMock = () => {
 	const benefitsMPMock = [
 		{
-			urlImgBackground: '/images/multicontent.jpg',
+			urlImgBackground: '/images/BenefitsMp/multicontent.jpg',
 			alt: 'Disney+ y Star+',
-			urlIcon: '/images/comboplus-square-multicontent.jpg',
+			urlIcon: '/images/BenefitsMp/comboplus-square-multicontent.jpg',
 			description:
 				'<p style="font-weight:bold">Sin cargo con el nivel 6</p> <p>Disney+ y Star+</p>',
 			bgColor:
 				'linear-gradient(0deg, rgb(8, 19, 48) 0%, rgba(8, 19, 48, 0) 100%);',
 		},
 		{
-			urlImgBackground: '/images/HBOMAX.jpg',
+			urlImgBackground: '/images/BenefitsMp/HBOMax.jpg',
 			alt: 'hbo max',
-			urlIcon: '/images/hbomax-icon.png',
+			urlIcon: '/images/BenefitsMp/hbomax-icon.png',
 			description: `<span>7 DÍAS GRATIS</span> <p style="font-weight:bold">Hasta 30% OFF</p> <p>HBO Max</p>`,
 			bgColor:
 				'linear-gradient(0deg, rgb(60, 3, 78) 0%, rgba(60, 3, 78, 0) 100%)',
 		},
 		{
-			urlImgBackground: '/images/paramount.jpg',
+			urlImgBackground: '/images/BenefitsMp/paramount.jpg',
 			alt: 'paramount+',
-			urlIcon: '/images/paramount-icon.png',
+			urlIcon: '/images/BenefitsMp/paramount-icon.png',
 			description:
 				'<span>7 DÍAS GRATIS</span> <p style="font-weight:bold">Hasta 30% OFF</p> <p>Paramount+</p>',
 			bgColor:
--- src/HomePage/components/Discover/api/getDiscoverOffers.ts
+++ src/HomePage/components/Discover/api/getDiscoverOffers.ts
@@ -5,23 +5,22 @@ const getDiscoverOffers = (): DiscoverOfferInterface[] => {
 		{
 			typeOffer: 'INFALTABLES DE INVIERNO',
 			titleOffer: 'HASTA 30% OFF Y 3 CUOTAS SIN INTERÉS',
-			urlImage:
-				'src/HomePage/components/Discover/images/infaltables-invierno.webp',
+			urlImage: '/images/DiscoverOffers/infaltables-invierno.webp',
 		},
 		{
 			typeOffer: 'ESPECIAL TVS',
 			titleOffer: 'HASTA 25% OFF Y 6 CUOTAS SIN INTERÉS',
-			urlImage: 'src/HomePage/components/Discover/images/especial-tv.webp',
+			urlImage: '/images/DiscoverOffers/especial-tv.webp',
 		},
 		{
 			typeOffer: 'ESPECIAL GAMING',
 			titleOffer: 'HASTA 30% OFF Y 6 CUOTAS SIN INTERÉS',
-			urlImage: 'src/HomePage/components/Discover/images/especial-gaming.webp',
+			urlImage: '/images/DiscoverOffers/especial-gaming.webp',
 		},
 		{
 			typeOffer: 'PEQUEÑOS ELECTRO',
 			titleOffer: 'HASTA 25% OFF Y 6 CUOTAS SIN INTERÉS',
-			urlImage: 'src/HomePage/components/Discover/images/pequeños-electro.webp',
+			urlImage: '/images/DiscoverOffers/pequeños-electro.webp',
 		},
 	]
 	return discoverOffersMock
